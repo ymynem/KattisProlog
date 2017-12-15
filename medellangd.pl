035cf960c4242d52d6816bf1c3e7a3b5f8737bdc
Monad <mona.dadoun@gmail.com>
2017-12-15 17:44:03 +0100

    Update medellangd.pl

--- medellangd.pl
+++ medellangd.pl
@@ -1,28 +1,28 @@
 %Medellängd
 medellangd([],0). 
 medellangd(Text, AvgLen):- 
-            analyse(Text, N, W), 
+            analysera(Text, N, W), 
             AvgLen is N/W, 
             !. % don´t backtrack
 
-analyse([], 0, 0).
-analyse([H|Text], N, W) :- 
+analysera([], 0, 0).
+analysera([H|Text], N, W) :- 
     char_type(H, alpha),
-    wordLength(Text, Word),
+    ordLangd(Text, Word),
     drop(Word, Text, Rest),
-    analyse(Rest, N1, W1),
+    analysera(Rest, N1, W1),
     N is N1+Word+1,
     W is W1+1.
 
-analyse([H|Text], N, W):-
-    not(char_type(H,alpha)), analyse(Text, N, W).
+analysera([H|Text], N, W):-
+    not(char_type(H,alpha)), analysera(Text, N, W).
 
-wordLength([],0).
-wordLength([H|T], N) :-
+ordLangd([],0).
+ordLangd([H|T], N) :-
     char_type(H, alpha),
-    wordLength(T, N1),
+    ordLangd(T, N1),
     N is N1+1.
-wordLength([H|_], 0):-
+ordLangd([H|_], 0):-
     not(char_type(H, alpha)).
 
 drop(_, [], []).
